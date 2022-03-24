81095a9cbc7fe9a71295195defd8bd0f2a143b59
PriyankaPerumal1 <90901203+PriyankaPerumal1@users.noreply.github.com>
2022-03-24 16:30:02 +0530

    Update test_frequent_range.py

--- test_frequent_range.py
+++ test_frequent_range.py
@@ -3,8 +3,13 @@ import frequent_range
 
 class FrequentRangeTest(unittest.TestCase):
     def test_get_frequent_range(self):
-        readings = [1,1,3,2]
-        self.assertTrue(frequent_range.get_frequent_range(readings) == '1-3, 4')
+        self.assertTrue(frequent_range.get_frequent_range([1,1,3,2]) == '1-3, 4')
+        self.assertTrue(frequent_range.get_frequent_range([5,1,1,2,4,3]) == '1-5, 6')
+        self.assertTrue(frequent_range.get_frequent_range([3,4,5,6,5,6,6,8,9]) == '3-6, 7\n8-9, 2')
+        self.assertTrue(frequent_range.get_frequent_range([1,2,5,6,7,8,10,11]) == '1-2, 2\n5-8, 4\n10-11, 2')
+        self.assertTrue(frequent_range.get_frequent_range([3]) == '')
+        self.assertTrue(frequent_range.get_frequent_range([2,5,9,13]) == '')
+        self.assertTrue(frequent_range.get_frequent_range([20,20,20,20]) == '')
 
 if __name__ == '__main__':
   unittest.main()
