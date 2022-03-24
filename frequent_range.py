f2429ee378d6f92786de1a90d04bfb359aeffe2f
PriyankaPerumal1 <90901203+PriyankaPerumal1@users.noreply.github.com>
2022-03-24 16:30:19 +0530

    Update frequent_range.py

--- frequent_range.py
+++ frequent_range.py
@@ -1,5 +1,23 @@
+from more_itertools import consecutive_groups
+
+
+def get_group_count(readings, group):
+    count = 0
+    for item in group:
+        count += readings.count(item)
+    return count
+
+def get_formatted_output(min, max, count):
+    return "{}-{}, {}".format(min,max,count)
+
 def get_frequent_range(readings):
+    frequent_range = ""
     readings.sort()
-    frequent_range = f'{readings[0]}-{readings[-1]}, {len(readings)}'
-    print(frequent_range)
-    return frequent_range
+    OrderedList = list(dict.fromkeys(readings))
+    
+    for group in consecutive_groups(OrderedList):
+        group = list(group)
+        if len(group) != 1:
+            frequent_range += get_formatted_output(group[0],group[-1],get_group_count(readings,group)) + "\n"
+    
+    return frequent_range.rstrip()
