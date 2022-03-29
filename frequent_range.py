711e429d7dd2d1d515d9193c1f602e9fc33c064b
PriyankaPerumal1 <90901203+PriyankaPerumal1@users.noreply.github.com>
2022-03-29 22:40:58 +0530

    Update frequent_range.py

--- frequent_range.py
+++ frequent_range.py
@@ -1,11 +1,12 @@
 from more_itertools import consecutive_groups
 
+A2D = {
+    '12Bit' : {'resolution' : 4094, 'maxCurrent' : 10, 'scalingFactor' : 0  },
+    '10Bit' : {'resolution' : 1022, 'maxCurrent' : 30, 'scalingFactor' : 15 },
+}
 
 def get_group_count(readings, group):
-    count = 0
-    for item in group:
-        count += readings.count(item)
-    return count
+    return sum([readings.count(item) for item in group])
 
 def get_formatted_output(min, max, count):
     return "{}-{}, {}".format(min,max,count)
@@ -13,11 +14,19 @@ def get_formatted_output(min, max, count):
 def get_frequent_range(readings):
     frequent_range = ""
     readings.sort()
-    OrderedList = list(dict.fromkeys(readings))
-    
-    for group in consecutive_groups(OrderedList):
+    groups = consecutive_groups(list(dict.fromkeys(readings)))
+    for group in groups:
         group = list(group)
         if len(group) != 1:
             frequent_range += get_formatted_output(group[0],group[-1],get_group_count(readings,group)) + "\n"
     
     return frequent_range.rstrip()
+
+def ignore_error_readings(readings,type):
+    return [reading for reading in readings if reading <= A2D[type]['resolution']]
+
+def convert_input_to_amps(input, type):
+    return abs(round((input * A2D[type]['maxCurrent'])/A2D[type]['resolution']) - A2D[type]['scalingFactor'])
+
+def A2D_convertor(readings,type):
+    return [convert_input_to_amps(reading,type) for reading in ignore_error_readings(readings,type)]
